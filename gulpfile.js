cf1c50f0aebf17e2865fc086f4c6cf5f33eed0a4
obemgcabazn <obemgghost@ya.ru>
2018-02-16 20:52:48 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp          = require('gulp'),
+    livereload    = require('gulp-livereload'),
+    sass          = require('gulp-sass'),
+    pug           = require('gulp-pug');
+
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch( 'frontend/*.pug', gulp.series( 'pug' ) );
+  gulp.watch( 'frontend/*.sass', gulp.series( 'sass' ) );
+  gulp.watch( 'public/*.html').on('change', livereload.changed);
+});
+
+gulp.task('pug', function (){
+  return gulp.src( 'frontend/index.pug' )
+    .pipe( pug(
+      {pretty: false}
+      ) )
+    .pipe( gulp.dest( 'public/' ) )
+});
+
+gulp.task('sass', function () {
+  return gulp.src( 'frontend/style.sass' )
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest( 'public/'));
+});
+
+gulp.task(
+  'default',
+  gulp.parallel(
+    'sass',
+    'pug',
+    'watch'
+  )
+);
